80e93f6cc4676313032ae35c3aa33496c3c3d4c6
Ryan Johns <jedimasterjohns@gmail.com>
2018-06-28 14:54:19 -0600

    Plz

--- server.js
+++ server.js
@@ -1,27 +1,15 @@
-//Install express server
-
 const express = require('express');
-
-const http = require('http');
-
-const path = require('path');
-
 const app = express();
-
-// Serve only the static files from the dist directory
-
+// Run the app by serving the static files
+// in the dist directory
 app.use(express.static(__dirname + '/dist'));
-
-app.get('*', (req, res) => {
-
-res.sendFile(path.join(__dirname, 'dist/index.html'));
-
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
+const path = require('path');
+// ...
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-
-const port = process.env.PORT || 8080;
-
-app.set(port);
-
-const server = http.createServer(app);
-
-server.listen(port, () => console.log('Running…'));
